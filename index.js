6986a798c1fe4256919f52a61106e19d2c93f86f
derzorngottes <amy.conchie@gmail.com>
2016-05-04 15:14:37 -0700

    Build out camera shop

---
+++ index.js
@@ -0,0 +1,33 @@
+var app = angular.module('camerashop-app', []);
+
+app.controller('cameraController', ['$scope', function cameraController($scope) {
+  $scope.sortBy = [
+    { id: 'Rating', val: 'rating' },
+    { id: 'Price', val: 'price' }
+  ];
+  $scope.sorting = $scope.sortBy[0].val;
+
+  $scope.cameras = [
+    {
+      title: 'Nikon D3100 DSLR',
+      image: 'http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg',
+      rating: 3.4,
+      price: 369.99,
+      onSale: true
+    },
+    {
+      title: 'Canon EOS 70D',
+      image: 'http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg',
+      rating: 2.0,
+      price: 1099.0,
+      onSale: false
+    },
+    {
+      title: 'Nikon D810A',
+      image:'http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg',
+      rating: 4.2,
+      price: 3796.95,
+      onSale: true
+    }
+  ];
+}]);
